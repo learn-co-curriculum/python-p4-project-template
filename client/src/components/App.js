4a1b880f4538c1f6f43867c94e9e0d4e95526e63
Matthew Longshore <matthewlongshore12@gmail.com>
2023-04-18 00:18:00 -0400

    basic react routes with first fetch

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,48 @@
-import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Farmers from './Farmers'
 
-function App() {
-  // Code goes here!
-}
+// Components for each route
+const Home = () => <h1>Home Page</h1>;
+// const Farmers = () => <h1>About Page</h1>;
+const Route3 = () => <h1>Route 3 Page</h1>;
+const Route4 = () => <h1>Route 4 Page</h1>;
+
+const App = () => {
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/farmers">Farmers</Link>
+          </li>
+          <li>
+            <Link to="/Route3">Route 3</Link>
+          </li>
+          <li>
+            <Link to="/Route4">Route 4</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/farmers">
+          <Farmers />
+        </Route>
+        <Route path="/Route3">
+          <Route3 />
+        </Route>
+        <Route path="/Route4">
+          <Route4 />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
---
+++ client/src/components/Farmers.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+
+function Farmers() {
+  const [farmers, setFarmers] = useState([]);
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:5555/farmers')
+      .then((response) => response.json())
+      .then((data) => setFarmers(data));
+  }, []);
+
+  return (
+    <div>
+      {farmers.map((farmer) => (
+        <div key={farmer.id}>
+            Name: {farmer.name}
+            <p>
+                --Location: {farmer.location}
+            </p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Farmers;
