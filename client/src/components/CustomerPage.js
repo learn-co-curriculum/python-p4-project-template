9d58506b2bbc588aef4f9e0fbd39e963dd62699e
Matthew Longshore <matthewlongshore12@gmail.com>
2023-04-19 17:51:04 -0400

    work please

--- client/src/components/CustomerPage.js
+++ client/src/components/CustomerPage.js
@@ -10,32 +10,44 @@ function CustomerPage() {
 
   useEffect(() => {
     if (customerName) {
-      fetch('http://127.0.0.1:5555/customers')
+      fetch('http://127.0.0.1:5555/orders')
         .then((response) => response.json())
         .then((data) => {
-          const customerOrders = data.filter((customer) => customer.name === customerName);
+          const customerOrders = data.filter((order) => order.customer.name === customerName);
           setOrders(customerOrders);
         });
     }
   }, [customerName]);
+
+  const handleDeleteOrder = (orderId) => {
+    fetch(`http://127.0.0.1:5555/orders/${orderId}`, {
+      method: 'DELETE'
+    })
+    .then(() => {
+      const newOrders = orders.filter((order) => order.id !== orderId);
+      setOrders(newOrders);
+    })
+    .catch((error) => console.error(error));
+  }
 
   return (
     <div>
       <FarmerNavBar customerName={customerName}/>
       {customerName ? (
         <>
-          <h2>Orders for {customerName}</h2>
+          <h2>Profile for {customerName}</h2>
           {orders.map((order) => (
             <div key={order.id}>
                 <p>
-                Item: {order.details}
+                Item: {order.details} -- Farmer: {order.farmer.name}
                 </p>
+                <button onClick={() => handleDeleteOrder(order.id)}>Delete</button>
             </div>
           ))}
         </>
-          ) : (
-            <h2>Please log in first</h2>
-        )}
+      ) : (
+        <h2>Please log in first</h2>
+      )}
     </div>
   );
 }
