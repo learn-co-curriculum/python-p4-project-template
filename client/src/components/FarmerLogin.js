0690adcfd7e9b2e4a110f39f7001408070ea97a3
Matthew Longshore <matthewlongshore12@gmail.com>
2023-04-19 01:39:11 -0400

    added farmers login finally

--- client/src/components/FarmerLogin.js
+++ client/src/components/FarmerLogin.js
@@ -1,14 +1,36 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import "./Home.css"
 
 function FarmerLogin() {
+  const [name, setName] = useState('');
+  const history = useHistory();
 
-    return(
-        <div className="header">
-            <h1 className="welcome">WELCOME!</h1>
-            <input className="fc" type="text" placeholder="Sign In"/>
-            <div className="drop-down-form">Create an account</div>
-        </div>
-    )
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    fetch('http://127.0.0.1:5555/farmers')
+      .then((response) => response.json())
+      .then((data) => {
+        const foundFarmer = data.find((farmer) => farmer.name === name);
+        if (foundFarmer) {
+          history.push('/farmers');
+        } else {
+          alert('Invalid farmer name');
+        }
+      });
+  }
+
+  return(
+    <div className="header">
+      <h1 className="welcome">WELCOME!</h1>
+      <form onSubmit={handleSubmit}>
+        <input className="fc" type="text" placeholder="Sign In" value={name} onChange={(e) => setName(e.target.value)} />
+        <button type="submit">Submit</button>
+      </form>
+      <div className="drop-down-form">Create an account</div>
+    </div>
+  )
 }
 
 export default FarmerLogin;
