ef69b152268fe698728da0cdfb5fc53a11d05eb9
Matthew Longshore <matthewlongshore12@gmail.com>
2023-04-20 15:01:27 -0400

    did add customer form

--- client/src/components/CustomerLogin.js
+++ client/src/components/CustomerLogin.js
@@ -5,6 +5,10 @@ import "./Home.css"
 function CustomerLogin() {
   const [name, setName] = useState('');
   const history = useHistory();
+  const [showForm, setShowForm] = useState(false);
+  const [newCustomerName, setNewCustomerName] = useState('');
+  const [newCustomerAddress, setNewCustomerAddress] = useState('');
+  const [newCustomerPaymentMethod, setNewCustomerPaymentMethod] = useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -27,6 +31,34 @@ function CustomerLogin() {
       });
   }
 
+  const handleNewCustomerSubmit = (event) => {
+    event.preventDefault();
+
+    fetch('http://127.0.0.1:5555/customers', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: newCustomerName,
+        address: newCustomerAddress,
+        payment_method: newCustomerPaymentMethod
+      })
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        alert(`Customer ${data.name} added successfully!`);
+        setNewCustomerName('');
+        setNewCustomerAddress('');
+        setNewCustomerPaymentMethod('');
+        setShowForm(false);
+      });
+  }
+
+  const handleNewCustomerCancel = () => {
+    setShowForm(false);
+  }
+
   return(
     <div className="header">
       <h1 className="welcome">WELCOME!</h1>
@@ -34,7 +66,16 @@ function CustomerLogin() {
         <input className="fc" type="text" placeholder="Sign In" value={name} onChange={(e) => setName(e.target.value)} />
         <button type="submit">Submit</button>
       </form>
-      <div className="drop-down-form">Become a new customer</div>
+      <div className="drop-down-form" onClick={() => setShowForm(true)}>Become a new customer</div>
+      {showForm && (
+        <form onSubmit={handleNewCustomerSubmit}>
+          <input type="text" placeholder="Name" value={newCustomerName} onChange={(e) => setNewCustomerName(e.target.value)} />
+          <input type="text" placeholder="Address" value={newCustomerAddress} onChange={(e) => setNewCustomerAddress(e.target.value)} />
+          <input type="text" placeholder="Payment Method" value={newCustomerPaymentMethod} onChange={(e) => setNewCustomerPaymentMethod(e.target.value)} />
+          <button type="submit">Add Customer</button>
+          <button type="button" onClick={handleNewCustomerCancel}>Cancel</button>
+        </form>
+      )}
     </div>
   )
 }
