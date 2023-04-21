dd7a99cbd30372c4341d28b5a15ecb0c264131f0
Matthew Longshore <matthewlongshore12@gmail.com>
2023-04-21 10:38:16 -0400

    CSS stuff work please

--- client/src/components/CustomerPage.js
+++ client/src/components/CustomerPage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import FarmerNavBar from './FarmerNavBar'
 import OrderForm from './OrderForm'
+import "./CustomerPage.css"
 
 function CustomerPage() {
   const [orders, setOrders] = useState([]);
